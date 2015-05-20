690bb90b7e92bd59e8574e63484b01229c79fb87
Victor Häggqvist <victor@snilius.com>
2015-05-21 01:46:35 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='giti',
+    version='0.2.0',
+    packages=[],
+    url='https://github.com/victorhaggqvist/giti',
+    scripts=['bin/giti'],
+    license='MIT',
+    author='Victor Häggqvist',
+    author_email='victor@snilius.com',
+    description='A python command line tool fetching getting .gitignore files'
+)
