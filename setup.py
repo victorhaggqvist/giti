2593794544c78ad89b1ac67576191bddad1a8f15
Victor Häggqvist <victor@snilius.com>
2015-10-15 14:11:45 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='giti',
-    version='0.2.0',
+    version='0.2.2',
     packages=[],
     url='https://github.com/victorhaggqvist/giti',
     scripts=['giti'],
